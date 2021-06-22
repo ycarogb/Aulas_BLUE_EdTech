ba001a902a77e5a466260329e04ed2fb43dd4a54
Ycaro <ycarobatalha1@gmail.com>
2021-06-22 13:17:46 -0300

    Exercicios Aula 08

--- Aula08/Exercicio_2/static/script.js
+++
@@ -1,8 +0,0 @@
-const contador = 0;
-const nomePokemon = document.getElementsByClassName("nome_pokemon");
-const textoPokemon = document.getElementById("pokemon_selecionado")
-
-function selecionaPokemon(){
-    contador += 1
-    textoPokemon.innerHTML = "<h3>Você selecionou " + contador + " pokemons!!</h3>"
-}
---
+++ Aula08/Exercicio_Pokemon1/static/script.js
@@ -0,0 +1,22 @@
+
+function iniciar(){
+    const personagens= document.querySelectorAll(".card_pokemon");
+    const texto = document.querySelector("#texto");
+    for (const personagem of personagens){
+        personagem.addEventListener("click" , marcarElementoSelecionado);
+    }
+}
+
+function marcarElementoSelecionado(){
+    const nomePokemon = this.getAttribute("data-nome")
+    
+    if(!this.classList.contains("selecionado")){
+        this.classList.add("selecionado");
+        texto.innerHTML = `Você selecionou <b>${nomePokemon}</b>`;
+    }
+    else {
+        this.classList.remove("selecionado");
+        texto.innerHTML = "Selecione um pokemon"
+    }
+
+}
